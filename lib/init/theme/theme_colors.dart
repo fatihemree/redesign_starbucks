fe095617453c9b1274935e1f084e633009992092
fatihemree <fatih.emre.212@gmail.com>
2021-12-03 13:15:48 +0300

    themeData

--- lib/init/theme/theme_colors.dart
+++ lib/init/theme/theme_colors.dart
@@ -3,15 +3,15 @@ import 'package:flutter/material.dart';
 class ThemeColors {
   // mainColor
   static Color white = const Color(0xffFFFFFF);
+  static Color background = const Color(0xffF7F8FA);
 
   //primary color
   static Color darkGreen = const Color(0xff1D4D4F);
   static Color mainGreen = const Color(0xff4AA366);
-  static Color greenLight = const Color(0xff4AA366);
   //grey
   static Color dark = const Color(0xff2E2D38);
   static Color darkGrey = const Color(0xff4D5661);
-  static Color grey = const Color(0xff6F8094);
+  static Color grey = const Color(0xffDFE4EC);
   static Color mediumGrey = const Color(0xffC4C4C4);
   static Color lightGrey = const Color(0xffC4C4C4);
   static Color softGrey = const Color(0xffF4F4F4);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './init/extension/context_extension.dart';
+import './init/theme/theme_colors.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,16 +9,46 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      theme: ThemeData(fontFamily: 'Inter'),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar',style: ,),
+      theme: ThemeData(
+        fontFamily: 'Inter',
+        colorScheme: ColorScheme(
+          background: ThemeColors.background,
+          onBackground: ThemeColors.dark,
+          primary: ThemeColors.mainGreen,
+          primaryVariant: ThemeColors.darkGreen,
+          onPrimary: Colors.white,
+          surface: ThemeColors.white,
+          onSurface: ThemeColors.dark,
+          secondary: ThemeColors.grey,
+          secondaryVariant: ThemeColors.darkGrey,
+          error: ThemeColors.darkRed,
+          onSecondary: ThemeColors.white,
+          onError: ThemeColors.darkRed,
+          brightness: Brightness.light,
         ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
+        appBarTheme: AppBarTheme(backgroundColor: Colors.white),
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+              fontSize: 72.0, color: Colors.blue, fontWeight: FontWeight.bold),
+          headline6: TextStyle(
+              fontSize: 36.0, color: Colors.amber, fontStyle: FontStyle.italic),
+          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
         ),
+      ),
+      home: Deneme(),
+    );
+  }
+}
+
+class Deneme extends StatelessWidget {
+  const Deneme({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        // child: Text('HelloWord', style: Theme.of(context).textTheme.headline1),
+        child: Text('HelloWord', style: context.textTheme.headline1),
       ),
     );
   }
