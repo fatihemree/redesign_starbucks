0f2a884675d5d5f23084857ec709fe32f5abb9d4
fatihemree <fatih.emre.212@gmail.com>
2021-12-03 11:05:38 +0300

    appbar & bottom navigation file create

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(fontFamily: 'Inter'),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          title: Text('Material App Bar',style: ,),
         ),
         body: Center(
           child: Container(
